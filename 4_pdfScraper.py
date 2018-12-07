275cc8dcdafd779fa29d706dd1a768b4ea91e5dd
Kazunori Hassy Hashikuchi <k.hashikuchi@gmail.com>
2018-12-08 01:09:36 +1300

    Store the result in csv

--- 4_pdfScraper.py
+++ 4_pdfScraper.py
@@ -4,6 +4,7 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from io import StringIO
 from io import open
+import csv
 
 
 def readPDF(pdfFile):
@@ -23,7 +24,31 @@ def readPDF(pdfFile):
 pdfFile = urlopen(
     'http://www.city.osaka.lg.jp/hodoshiryo/cmsfiles/contents/0000392/392906/H29-3-suikei.pdf')
 outputString = readPDF(pdfFile)
-
-# 適宜整形してやる必要はある……
 print(outputString)
+
+
+def filterEmptyValue(word):
+    return word != ''
+
+
+words = outputString.split('\n')
+words = list(filter(filterEmptyValue, words))  # 空の行を削除
+
+# CSVに整形する
+end = words.index("（単位：人）")
+data = [["区名", "総数", "男", "女", "面積", "人口密度", "世帯数", "世帯数増減", "前月人口"]]
+
+# データを9行ずつ取得してグルーピング
+for i in range(8, end, 9):
+    tempData = []
+    for j in range(0, 9):
+        tempData.append(words[i + j])
+    data.append(tempData)
+
 pdfFile.close()
+
+# CSVに書き込む
+csvFile = open("osaka.csv", "w")
+with csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(data)
